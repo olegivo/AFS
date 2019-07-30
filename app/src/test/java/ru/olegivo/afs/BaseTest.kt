9783ef610640a229fa7f7e0da3501852206a1b7e
olegivo <olegivo@gmail.com>
2019-07-30 17:22:06 +0300

    added BaseTest

---
+++ app/src/test/java/ru/olegivo/afs/BaseTest.kt
@@ -0,0 +1,27 @@
+package ru.olegivo.afs
+
+import com.nhaarman.mockitokotlin2.reset
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import ru.olegivo.afs.domain.RxSchedulerRule
+
+abstract class BaseTest {
+    @Rule
+    @JvmField
+    val schedulerRule = RxSchedulerRule()
+    private val mocks: Array<Any> by lazy { getAllMocks() }
+    protected abstract fun getAllMocks(): Array<Any>
+
+    @Before
+    fun setUp() {
+        if(mocks.isNotEmpty()) reset(*mocks)
+        schedulerRule.testScheduler.triggerActions()
+    }
+
+    @After
+    fun tearDown() {
+        if(mocks.isNotEmpty()) verifyNoMoreInteractions(*mocks)
+    }
+}
