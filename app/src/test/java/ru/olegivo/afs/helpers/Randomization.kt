a18a452248107c6945ed1d98102df76a66572a73
olegivo <olegivo@gmail.com>
2019-07-30 17:23:01 +0300

    added Randomization

---
+++ app/src/test/java/ru/olegivo/afs/helpers/Randomization.kt
@@ -0,0 +1,13 @@
+package ru.olegivo.afs.helpers
+
+import kotlin.random.Random
+
+private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+fun getRandomString(lenght: Int? = 4, minLength: Int = 1, maxLength: Int = 10): String {
+    val length = lenght ?: (Random.nextInt(minLength, maxLength))
+    return (1..length)
+        .map { Random.nextInt(0, charPool.size) }
+        .map(charPool::get)
+        .joinToString("")
+}
