620bcbd0ef0756229a5d08436add09b4dfa66ec5
olegivo <olegivo@gmail.com>
2019-08-19 17:52:26 +0300

    WIP choose club dialog

--- app/src/main/java/ru/olegivo/afs/MainActivity.kt
+++ app/src/main/java/ru/olegivo/afs/MainActivity.kt
@@ -1,12 +1,47 @@
 package ru.olegivo.afs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import dagger.android.AndroidInjection
+import io.reactivex.Scheduler
+import kotlinx.android.synthetic.main.activity_main.activity_main_choose_club
+import ru.olegivo.afs.clubs.android.ChooseClubDialog
+import ru.olegivo.afs.clubs.domain.GetClubsUseCase
+import ru.olegivo.afs.clubs.domain.models.Club
+import javax.inject.Inject
+import javax.inject.Named
+
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var getClubs: GetClubsUseCase
+
+    @field:[Inject Named("main")]
+    lateinit var mainScheduler: Scheduler
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        activity_main_choose_club.setOnClickListener {
+            onChooseClubClicked()
+        }
+    }
+
+    private fun onChooseClubClicked() {
+        getClubs()
+            .observeOn(mainScheduler)
+            .subscribe(
+                { clubs ->
+                    val selectedClub = clubs.random()
+                    ChooseClubDialog.chooseClub(clubs, selectedClub, this) { club -> setCurrentClub(club) }
+                },
+                {})
+    }
+
+    private fun setCurrentClub(club: Club) {
+        Toast.makeText(this, "Current club is ${club.title}", Toast.LENGTH_LONG).show()
     }
 }
