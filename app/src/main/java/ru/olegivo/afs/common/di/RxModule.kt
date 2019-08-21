8d7ee758a46c9cd204e371f9cd95010f9f6cf48f
olegivo <olegivo@gmail.com>
2019-08-21 14:27:47 +0300

    provide AndroidSchedulers.mainThread() dependency

--- app/src/main/java/ru/olegivo/afs/common/di/RxModule.kt
+++ app/src/main/java/ru/olegivo/afs/common/di/RxModule.kt
@@ -3,6 +3,7 @@ package ru.olegivo.afs.common.di
 import dagger.Module
 import dagger.Provides
 import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Named
 
@@ -15,5 +16,10 @@ class RxModule {
         @Provides
         @Named("io")
         fun provideIoScheduler(): Scheduler = Schedulers.io()
+
+        @JvmStatic
+        @Provides
+        @Named("main")
+        fun provideMainScheduler(): Scheduler = AndroidSchedulers.mainThread()
     }
 }
