985b1ddeffb036a97c434c59209848460363225f
olegivo <olegivo@gmail.com>
2019-08-29 14:31:17 +0300

    repair ClubsNetworkSourceImplTest (schedulers inject)

--- app/src/androidTest/java/ru/olegivo/afs/clubs/network/ClubsNetworkSourceImplTest.kt
+++ app/src/androidTest/java/ru/olegivo/afs/clubs/network/ClubsNetworkSourceImplTest.kt
@@ -1,5 +1,6 @@
 package ru.olegivo.afs.clubs.network
 
+import io.reactivex.schedulers.TestScheduler
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import ru.olegivo.afs.common.network.AuthorizedApiTest
@@ -7,9 +8,15 @@ import ru.olegivo.afs.common.network.AuthorizedApiTest
 class ClubsNetworkSourceImplTest : AuthorizedApiTest() {
     @Test
     fun getClubs() {
-        val clubs = ClubsNetworkSourceImpl(api)
+        val scheduler = TestScheduler()
+
+        val testObserver = ClubsNetworkSourceImpl(api, scheduler)
             .getClubs()
             .test()
+
+        scheduler.triggerActions()
+
+        val clubs = testObserver
             .assertNoErrors()
             .values()
             .single()
