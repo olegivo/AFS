c02a7589b391d07223d8647a4faf35452a9b3335
olegivo <olegivo@gmail.com>
2019-09-11 14:20:47 +0300

    added Randomizations for Long and Date

--- app/src/test/java/ru/olegivo/afs/helpers/Randomization.kt
+++ app/src/test/java/ru/olegivo/afs/helpers/Randomization.kt
@@ -1,5 +1,6 @@
 package ru.olegivo.afs.helpers
 
+import java.util.*
 import kotlin.random.Random
 
 private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
@@ -15,3 +16,4 @@ fun getRandomString(lenght: Int? = 4, minLength: Int = 1, maxLength: Int = 10): 
 fun getRandomBoolean() = Random.nextBoolean()
 fun getRandomInt() = Random.nextInt()
 fun getRandomLong() = Random.nextLong()
+fun getRandomDate() = Date(getRandomLong())
