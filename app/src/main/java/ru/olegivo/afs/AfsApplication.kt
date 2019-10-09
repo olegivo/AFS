ffacda039ea57b1bd78e601fccca930bfae4efff
olegivo <olegivo@gmail.com>
2019-10-09 17:12:32 +0300

    Add debug logging

--- app/src/main/java/ru/olegivo/afs/AfsApplication.kt
+++ app/src/main/java/ru/olegivo/afs/AfsApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
 import ru.olegivo.afs.common.di.DaggerAppComponent
+import timber.log.Timber
 import javax.inject.Inject
 
 class AfsApplication : Application(), HasAndroidInjector {
@@ -12,6 +13,10 @@ class AfsApplication : Application(), HasAndroidInjector {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG_MODE) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         DaggerAppComponent.factory().create(this).inject(this)
     }
---
+++ app/src/main/java/ru/olegivo/afs/UncaughtException.kt
@@ -0,0 +1,43 @@
+package ru.olegivo.afs
+
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
+import timber.log.Timber
+import java.io.IOException
+import java.net.SocketException
+
+class UncaughtException internal constructor() : Thread.UncaughtExceptionHandler {
+    private val rootHandler: Thread.UncaughtExceptionHandler
+
+    init {
+        Timber.tag(javaClass.simpleName)
+        rootHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler(this)
+        RxJavaPlugins.setErrorHandler(this::onRxError)
+    }
+
+    override fun uncaughtException(t: Thread, e: Throwable) {
+        Timber.e(e, "Uncaught Exception in t '%s'", t.name)
+        rootHandler.uncaughtException(t, e)
+    }
+
+    private fun onRxError(ex: Throwable) {
+        val e = if (ex is UndeliverableException) ex.cause!! else ex
+
+        when (e) {
+            is IOException, is SocketException, /*fine, irrelevant network problem or API that throws on cancellation*/
+            is InterruptedException /*fine, some blocking code was interrupted by a dispose call*/ -> {
+            }
+
+            is NullPointerException, is IllegalArgumentException /*that's likely a bug in the application*/,
+            is IllegalStateException /*that's a bug in RxJava or in a custom operator*/ -> {
+                Thread.currentThread().uncaughtExceptionHandler.uncaughtException(Thread.currentThread(), e)
+            }
+
+            else -> {
+                Timber.w(e, "Undeliverable exception received, not sure what to do")
+            }
+        }
+    }
+}
