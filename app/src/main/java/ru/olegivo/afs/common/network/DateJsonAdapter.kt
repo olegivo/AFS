40d1cf2f5a4d2858dccc515c7374ffe36d5d13da
olegivo <olegivo@gmail.com>
2019-12-13 16:48:59 +0300

    remove deprecated com.google.gson.internal.bind.util.ISO8601Utils

--- app/src/main/java/ru/olegivo/afs/common/network/DateJsonAdapter.kt
+++ app/src/main/java/ru/olegivo/afs/common/network/DateJsonAdapter.kt
@@ -1,6 +1,5 @@
 package ru.olegivo.afs.common.network
 
-import com.google.gson.internal.bind.util.ISO8601Utils
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonReader
 import com.squareup.moshi.JsonWriter
@@ -14,14 +13,12 @@ class DateJsonAdapter : JsonAdapter<Date>() {
     @Throws(IOException::class)
     override fun fromJson(reader: JsonReader): Date {
         val timeStamp = reader.nextString()
-        // ISO8601Utils.parse(timeStamp, ParsePosition(0))
         return DateTime(timeStamp).toDate()
     }
 
     @Synchronized
     @Throws(IOException::class)
     override fun toJson(writer: JsonWriter, value: Date?) {
-        val string = ISO8601Utils.format(value)
-        writer.value(string)
+        value?.let { writer.value(it.toString()) }
     }
 }
