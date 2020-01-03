16842f5391b15edd4d67c5db20e61b6e5372222d
olegivo <olegivo@gmail.com>
2020-01-03 17:46:26 +0300

    Plan favorite reminder only if record date not passed

--- app/src/main/java/ru/olegivo/afs/favorites/domain/PlanFavoriteRecordReminderUseCaseImpl.kt
+++ app/src/main/java/ru/olegivo/afs/favorites/domain/PlanFavoriteRecordReminderUseCaseImpl.kt
@@ -1,18 +1,29 @@
 package ru.olegivo.afs.favorites.domain
 
 import io.reactivex.Completable
+import ru.olegivo.afs.common.domain.DateProvider
+import ru.olegivo.afs.extensions.toSingle
 import ru.olegivo.afs.schedules.domain.models.Schedule
 import javax.inject.Inject
 
 class PlanFavoriteRecordReminderUseCaseImpl @Inject constructor(
     private val favoritesRepository: FavoritesRepository,
-    private val favoriteAlarmPlanner: FavoriteAlarmPlanner
+    private val favoriteAlarmPlanner: FavoriteAlarmPlanner,
+    private val dateProvider: DateProvider
 ) : PlanFavoriteRecordReminderUseCase {
 
     override fun invoke(schedule: Schedule): Completable =
-        favoritesRepository.addReminderToRecord(schedule)
-            .andThen(Completable.defer {
-                favoriteAlarmPlanner.planFavoriteRecordReminder(schedule)
-            })
+        { dateProvider.getDate() }.toSingle()
+            .flatMapCompletable { now ->
+                val threshold = schedule.recordTo ?: schedule.datetime
+                if (threshold > now) {
+                    favoritesRepository.addReminderToRecord(schedule)
+                        .andThen(Completable.defer {
+                            favoriteAlarmPlanner.planFavoriteRecordReminder(schedule)
+                        })
+                } else {
+                    Completable.complete()
+                }
+            }
 
 }
--- app/src/test/java/ru/olegivo/afs/favorites/domain/PlanFavoriteRecordReminderUseCaseImplTest.kt
+++ app/src/test/java/ru/olegivo/afs/favorites/domain/PlanFavoriteRecordReminderUseCaseImplTest.kt
@@ -7,24 +7,36 @@ import com.nhaarman.mockitokotlin2.willReturn
 import io.reactivex.Completable
 import org.junit.Test
 import ru.olegivo.afs.BaseTestOf
+import ru.olegivo.afs.common.add
+import ru.olegivo.afs.common.domain.DateProvider
 import ru.olegivo.afs.schedules.domain.models.createSchedule
+import java.util.*
 
 class PlanFavoriteRecordReminderUseCaseImplTest : BaseTestOf<PlanFavoriteRecordReminderUseCase>() {
     override fun createInstance() =
-        PlanFavoriteRecordReminderUseCaseImpl(favoritesRepository, favoriteAlarmPlanner)
+        PlanFavoriteRecordReminderUseCaseImpl(
+            favoritesRepository,
+            favoriteAlarmPlanner,
+            dateProvider
+        )
 
     private val favoritesRepository: FavoritesRepository = mock()
     private val favoriteAlarmPlanner: FavoriteAlarmPlanner = mock()
+    private val dateProvider: DateProvider = mock()
 
     override fun getAllMocks() = arrayOf(
         favoritesRepository,
-        favoriteAlarmPlanner
+        favoriteAlarmPlanner,
+        dateProvider
     )
 
     @Test
-    fun `invoke `() {
-        val schedule = createSchedule()
+    fun `invoke PLANS reminder WHEN recordTo not passed`() {
+        val recordTo = Date()
+        val schedule = createSchedule().copy(recordTo = recordTo)
+        val now = recordTo.add(hours = -1)
 
+        given { dateProvider.getDate() }.willReturn { now }
         given { favoritesRepository.addReminderToRecord(schedule) }
             .willReturn { Completable.complete() }
         given { favoriteAlarmPlanner.planFavoriteRecordReminder(schedule) }
@@ -32,8 +44,57 @@ class PlanFavoriteRecordReminderUseCaseImplTest : BaseTestOf<PlanFavoriteRecordR
 
         instance.invoke(schedule)
             .assertSuccess()
-        
+
+        verify(dateProvider).getDate()
         verify(favoritesRepository).addReminderToRecord(schedule)
         verify(favoriteAlarmPlanner).planFavoriteRecordReminder(schedule)
+    }
+
+    @Test
+    fun `invoke PLANS reminder WHEN recordTo not specified and start time not passed`() {
+        val datetime = Date()
+        val schedule = createSchedule().copy(recordTo = null, datetime = datetime)
+        val now = datetime.add(hours = -1)
+
+        given { dateProvider.getDate() }.willReturn { now }
+        given { favoritesRepository.addReminderToRecord(schedule) }
+            .willReturn { Completable.complete() }
+        given { favoriteAlarmPlanner.planFavoriteRecordReminder(schedule) }
+            .willReturn { Completable.complete() }
+
+        instance.invoke(schedule)
+            .assertSuccess()
+
+        verify(dateProvider).getDate()
+        verify(favoritesRepository).addReminderToRecord(schedule)
+        verify(favoriteAlarmPlanner).planFavoriteRecordReminder(schedule)
+    }
+
+    @Test
+    fun `invoke DOES nothing WHEN recordTo passed`() {
+        val recordTo = Date()
+        val schedule = createSchedule().copy(recordTo = recordTo)
+        val now = recordTo.add(hours = 1)
+
+        given { dateProvider.getDate() }.willReturn { now }
+
+        instance.invoke(schedule)
+            .assertSuccess()
+
+        verify(dateProvider).getDate()
+    }
+
+    @Test
+    fun `invoke DOES nothing WHEN recordTo not specified and start time passed`() {
+        val datetime = Date()
+        val schedule = createSchedule().copy(recordTo = null, datetime = datetime)
+        val now = datetime.add(hours = 1)
+
+        given { dateProvider.getDate() }.willReturn { now }
+
+        instance.invoke(schedule)
+            .assertSuccess()
+
+        verify(dateProvider).getDate()
     }
 }
