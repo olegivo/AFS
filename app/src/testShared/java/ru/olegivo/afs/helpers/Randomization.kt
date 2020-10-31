4dd5d492eb3b283ad6d4619f46a0433ff3627894
olegivo <olegivo@gmail.com>
2020-10-31 21:04:08 +0300

    add prefix and postfix to getRandomString

--- app/src/testShared/java/ru/olegivo/afs/helpers/Randomization.kt
+++ app/src/testShared/java/ru/olegivo/afs/helpers/Randomization.kt
@@ -22,12 +22,18 @@ import kotlin.random.Random
 
 private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 
-fun getRandomString(lenght: Int? = 4, minLength: Int = 1, maxLength: Int = 10): String {
+fun getRandomString(
+    lenght: Int? = 4,
+    minLength: Int = 1,
+    maxLength: Int = 10,
+    prefix: String = "",
+    postfix: String = ""
+): String {
     val length = lenght ?: (Random.nextInt(minLength, maxLength))
     return (1..length)
         .map { Random.nextInt(0, charPool.size) }
         .map(charPool::get)
-        .joinToString("")
+        .joinToString(separator = "", prefix = prefix, postfix = postfix)
 }
 
 fun getRandomBoolean() = Random.nextBoolean()
